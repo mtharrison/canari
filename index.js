9ecee77b66e8f9cd1195171ef8d23e6fb419b4c7
Matt Harrison <hi@matt-harrison.com>
2015-05-05 22:42:40 +0800

    Separated into smaller components

---
+++ index.js
@@ -0,0 +1,18 @@
+var Commander = require('commander');
+var Canari = require('./lib/canari');
+var Path = require('path');
+var Fs = require('fs');
+
+
+Commander.version(require('./package.json').Version)
+    .option('-f, --file <file>', 'Configuration file (required)')
+    .parse(process.argv);
+
+if (!Commander.file) {
+    console.error('Error: You must supply a configuration file argument with: --file filename.json');
+    process.exit(1);
+} 
+
+var config = JSON.parse(Fs.readFileSync(Path.resolve(Commander.file)));
+
+new Canari(config).start();
